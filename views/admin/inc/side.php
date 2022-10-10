96c48482f267f562c815d01c803c2da9f2bd47de
Brian Barasa <47526933+barasabwb@users.noreply.github.com>
2022-10-10 16:23:15 +0300

    Side Menu links

---
+++ views/admin/inc/side.php
@@ -0,0 +1,20 @@
+ <li class="nk-menu-item has-sub">
+                        <a href="#" class="nk-menu-link nk-menu-toggle">
+                            <span class="nk-menu-icon"><em class="icon ni ni-ticket-alt"></em></span>
+                            <span class="nk-menu-text">Travel Requests</span>
+                        </a>
+                        <ul class="nk-menu-sub">
+                            <li class="nk-menu-item">
+                                <a href="<?= URLROOT ?>/corporate/corporate_travel_requests/pending" class="nk-menu-link"><span class="nk-menu-text">Travel Requests</span></a>
+                            </li>
+                            <li class="nk-menu-item">
+                                <a  href="<?= URLROOT ?>/corporate/corporate_travel_requests/approved" class="nk-menu-link"><span class="nk-menu-text">Approved Requests</span></a>
+                            </li>
+                            <li class="nk-menu-item">
+                                <a  href="<?= URLROOT ?>/corporate/corporate_travel_requests/booked" class="nk-menu-link"><span class="nk-menu-text">Booked Travels</span></a>
+                            </li>
+                            <li class="nk-menu-item">
+                                <a  href="<?= URLROOT ?>/corporate/corporate_travel_requests/rejected" class="nk-menu-link"><span class="nk-menu-text">Rejected Requests</span></a>
+                            </li>
+                        </ul><!-- .nk-menu-sub -->
+                    </li><!-- .nk-menu-item -->
