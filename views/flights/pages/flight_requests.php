1dd207c4bf30c375a708b8f59b5ac358a8e6a0e0
Brian Barasa <47526933+barasabwb@users.noreply.github.com>
2022-10-10 16:25:14 +0300

    Search Requests

---
+++ views/flights/pages/flight_requests.php
@@ -0,0 +1,219 @@
+
+<!-- SAMPLE OFFER, MODALS ARE AT THE END -->
+    <div style="" data-v-48096888="" class="offers_airline col-12 md:col-6 lg:col-4 airline_KQ">
+        <article id="offer_1" data-v-dcfd84d8="" data-v-48096888="" class="offer_section group ticket relative border border-dark-20 rounded shadow bg-white hover:shadow-md duration-300 ease-out transition-all mb-40 pt-5"><span class="absolute right-0 mr-10 text-11">Expires in <span class="text-red time_to_expire_one">58</span> minutes</span>
+            <header data-v-dcfd84d8="" class="pt-20 px-20">
+
+                <div data-v-dcfd84d8="" class="flex justify-between">
+                    <div data-v-dcfd84d8="" class="text-25 leading-none font-medium">
+                        <span class="quote_amount"><span style="font-size:17px">KES</span> 9,932</span>
+
+                    </div>
+                    <div data-v-dcfd84d8="" class="relative">
+                        <p data-v-dcfd84d8="" class="md:block hidden carbon group-hover:opacity-0 flex items-baseline text-gray-50 text-10 leading-none absolute top-0 right-0 duration-300 ease-out transition-all">
+                            <!--
+                            <svg data-v-dcfd84d8="" xmlns="http://www.w3.org/2000/svg" class="icon-12 fill-gray-50 mr-5 icon sprite-icons">
+                                <use data-v-dcfd84d8="" href="https://travelduqa.duqa.africa/dist/web/f_assets/icons/7972d3521001c3595dc78cfce1baba60.svg#i-eco" xlink:href="https://travelduqa.duqa.africa/dist/web/f_assets/icons/7972d3521001c3595dc78cfce1baba60.svg#i-eco"></use>
+                            </svg>--><!--CO<sub data-v-dcfd84d8="">2</sub> <span data-v-dcfd84d8="" class="co2 ml-5">Kg</span>-->
+                        </p>
+                        <a href="" id="off_0000AN9OwxLXYB1KgULgzS" data-v-dcfd84d8="" type="duffel" class="text-white btn-select btn_request_offer select_offers relative z-2 md:opacity-0 group-hover:opacity-100 whitespace-no-wrap duration-100 ease-out transition-all btn btn-red">
+                            <i class="fa fa-send"></i>&nbsp;Request Travel
+                        </a>
+                        <textarea type="hidden" hidden="hidden" class="flight_data" style=""></textarea>
+
+                        <textarea type="hidden" hidden="hidden" class="passengers_data" style="">{"offer_id":"off_0000AN9OwxLXYB1KgULgzS","total":9306.154000000000451109372079372406005859375,"currency":"KES","passengers":[{"type":"adult","id":"pas_0000AN9OwlMA82O7UwY1IS"}]}</textarea>
+                    </div>
+                </div>
+                <!---->
+            </header>
+
+            <main data-v-dcfd84d8="">
+                <input type="hidden" class="time_to_expire" value="58 m">
+                <input type="hidden" class="expires_at" value="2022-09-02T11:13:04.683579Z">
+                <input type="hidden" class="created_at" value="2022-09-02T10:13:04.834599Z">
+
+                <section data-v-dcfd84d8="" class="mt-5 p-20 main_section">
+                    <div data-v-dcfd84d8="" class="relative z-2 flex items-center justify-between leading-none mb-20">
+                        <div data-v-dcfd84d8="" class="text-14 font-medium">
+                            Going
+
+                            <span data-v-dcfd84d8="" class="font-regular opacity-50" style="display: block;margin-top: 4px;">
+															<span class="departure-date-gtb">Fri 23, Sep</span> -
+															<span class="arrival-date-gtb">Fri 23, Sep</span>
+														</span>
+                        </div>
+                        <div data-v-dcfd84d8="" class="flex items-center text-14 font-medium" style="margin-top:-1px;display: block;">
+                            Total Time
+                            <span class="font-regular opacity-50 quote_duration_gtb" style="display: block;margin-top: 4px;">1h
+														</span>
+                        </div>
+                    </div>
+
+                    <section data-v-2fa65931="" data-v-dcfd84d8="" class="section-content-drop section-content border border-dark-20 rounded duration-300 transition-all ease-out hover:border-dark-60 cursor-pointer overflow-hidden height_270">
+                        <header data-v-2fa65931="" class="relative z-2 p-20 flex items-center offer_head">
+                            <img data-v-2fa65931="" title="Kenya Airways" src="https://res.cloudinary.com/dili9mwxz/image/upload/airlines/KQ.png" alt="Kenya Airways logo" class="quote_flight_image_otb rounded-sm image_airline" style="width:17%;margin-left:-10px;padding-right:18px;margin-right:10px;">
+                            <div data-v-2fa65931="" class="leading-none">
+                                <p data-v-2fa65931="" class="quote_dep_time_gtb text-14 font-medium whitespace-no-wrap">
+                                    06:30
+                                </p>
+                                <p data-v-2fa65931="" class="text-11 quote_departure_city_gtb opacity-60 mt-4">
+                                    Nairobi
+                                </p>
+                            </div><div data-v-2fa65931="" class="stops">
+                                <p data-v-2fa65931="" class="quote_status_otb text-12 font-medium px-5 bg-white text-center text-success">
+                                    Nonstop
+                                </p>
+                            </div><div data-v-2fa65931="" class="leading-none text-right">
+                                <p data-v-2fa65931="" class="quote_arr_time_gtb relative text-14 font-medium whitespace-no-wrap">
+                                    07:30
+                                </p>
+                                <p data-v-2fa65931="" class="text-11 opacity-60 mt-4 quote_arrival_city_gtb">
+                                    Mombasa
+                                </p>
+                            </div>
+                            <div data-v-2fa65931="" class="ml-15">
+                                <svg data-v-2fa65931="" xmlns="http://www.w3.org/2000/svg" class="icon-12 fill-dark duration-300 transition-all ease-out icon sprite-icons">
+                                    <use data-v-2fa65931="" href="https://travelduqa.duqa.africa/dist/web/f_assets/icons/7972d3521001c3595dc78cfce1baba60.svg#i-chevron" xlink:href="https://travelduqa.duqa.africa/dist/web/f_assets/icons/7972d3521001c3595dc78cfce1baba60.svg#i-chevron"></use>
+                                </svg>
+                            </div>
+                        </header>
+
+                        <main style="" data-v-2fa65931="" class="offer_footer wrapper bg-light text-12 leading-none rounded-b w-full duration-200 transition-all ease-in-out is-active height_270">
+                            <div data-v-2fa65931="" class="p-20 relative border-b border-dashed border-dark-20 pb-25">
+                                <div data-v-2fa65931="" class="flex justify-between">
+                                    <div data-v-2fa65931="">
+
+                                        <p data-v-2fa65931="" class="font-medium mb-2">
+                                            Operating Airline
+                                        </p>
+                                        <p data-v-2fa65931="" class="quote_airline_gtb opacity-60 whitespace-nowrap">
+                                            Kenya Airways
+                                        </p>
+                                    </div>
+                                    <div data-v-2fa65931="" class="text-right">
+                                        <p data-v-2fa65931="" class="font-medium mb-2">
+                                            Flight number
+                                        </p>
+                                        <p data-v-2fa65931="" class="quote_flight_no_gtb opacity-60 whitespace-nowrap">
+                                            KQ_600
+                                        </p>
+                                    </div>
+                                </div>
+                                <div data-v-2fa65931="" class="flex justify-between mt-10">
+                                    <div data-v-2fa65931="">
+                                        <p data-v-2fa65931="" class="font-medium mb-2">
+                                            Duration
+                                        </p>
+                                        <p data-v-2fa65931="" class=" opacity-60 whitespace-nowrap">
+                                            1h
+                                        </p>
+                                    </div>
+                                    <div data-v-2fa65931="" class="text-right">
+
+                                        <p data-v-2fa65931="" class="font-medium mb-2">
+                                            Class
+                                        </p>
+                                        <p data-v-2fa65931="" class="opacity-60 flight_class_gtb whitespace-nowrap">
+                                            Economy Class
+                                        </p>
+                                    </div>
+                                </div>
+                                <div data-v-2fa65931="" class="flex justify-between mt-10">
+                                    <div data-v-2fa65931="">
+                                        <p data-v-2fa65931="" class="font-medium mb-2">
+                                            Departure Airport
+                                        </p>
+                                        <p data-v-2fa65931="" class="quote_departure_airport_gtb opacity-60 whitespace-nowrap">
+                                            Jomo Kenyatta International
+                                        </p>
+                                    </div>
+                                    <div data-v-2fa65931="" class="text-right">
+                                        <p data-v-2fa65931="" class="font-medium mb-3">
+                                            Arrival Airport
+                                        </p>
+                                        <p data-v-2fa65931="" class="quote_arrival_airport_gtb opacity-60 whitespace-nowrap">
+                                            Moi International
+                                        </p>
+                                    </div>
+                                </div>
+                                <div data-v-2fa65931="" class="flex justify-between mt-10">
+                                    <div data-v-2fa65931="">
+                                        <p data-v-2fa65931="" class="font-medium mb-2">
+                                            Departure Time
+                                        </p>
+                                        <p data-v-2fa65931="" class="quote_departure_time_gtb opacity-60 whitespace-nowrap">
+                                            06:30
+                                        </p>
+                                    </div>
+                                    <div data-v-2fa65931="" class="text-right">
+                                        <p data-v-2fa65931="" class="font-medium mb-3">
+                                            Arrival Time
+                                        </p>
+                                        <p data-v-2fa65931="" class="quote_arrival_time_gtb opacity-60 whitespace-nowrap">
+                                            07:30
+                                        </p>
+                                    </div>
+                                </div>
+                                <div data-v-2fa65931="" class="stop">
+                                    <p data-v-2fa65931="" class="leading-none font-medium  text-red text-12 px-3">Mombasa</p>
+                                </div></div></main>
+                    </section>
+                </section></main>
+
+        </article>
+
+    </div>
+
+<!-- MODALS USED -->
+<!-- The Request travel Modal -->
+<div class="modal" id="request_offer_modal">
+    <div class="modal-dialog  modal-lg">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h2 class="modal-title"><i class="fa fa-send"></i>&nbsp;Travel Request</h2>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+                <br>
+                <div class="offer__body">
+					<span class="offer__details">
+					</span>
+                </div>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary btn-sm text-white h-30 submit_user_request_btn"><span class="button_icon"><i class="fa fa-send"></i></span>&nbsp;<span class="button_text">Request Travel</span></button>
+                <button type="button" class="btn btn-trans btn-sm h-30" data-dismiss="modal">Close</button>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="modal" id="confirm_travel_request">
+    <div class="modal-dialog  modal-md">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h2 class="modal-title"><i class="fa fa-send"></i>&nbsp;Confirm Travel Request</h2>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+                <div class="body_message text-center mb-2" style="margin-bottom:20px">
+                    You are about to submit a travel request to your Corporate for the offer you have selected. Do you want to proceed with submitting the request?
+                </div>
+            </div>
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary btn-sm text-white h-30 confirm_request_submission_btn"><span class="button_icon"><i class="fa fa-send"></i></span>&nbsp;<span class="button_text">Confirm Request</span></button>
+                <button type="button" class="btn btn-trans btn-sm h-30" data-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
